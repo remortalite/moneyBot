7a13ff095267dcf5a3d4a575a6b77e80dbbbae96
Ignat Mora <remortalite@gmail.com>
2023-02-22 20:12:10 +0700

    Sep functions to the different files

---
+++ messages.py
@@ -0,0 +1,39 @@
+import logging
+
+logging.getLogger(__name__)
+
+
+available_commands = dict() # "name : description"
+
+
+def create_commands() -> None:
+    d = dict()
+    d['help'] = 'print this message'
+    d['get'] = 'send all the wastings'
+
+    available_commands.update(d)
+
+
+def help_message():
+    """ Reply message to /help command
+    :returns: string with a help message
+    """
+
+    create_commands()
+
+    help_string = ""
+    for k, v in available_commands.items():
+        help_string += f"/{k} -- {v}\n"
+    logging.info(f"Help message: '{help_string}'")
+    return help_string
+
+
+def start_message():
+    """ Reply message to /start command
+    :returns: string with a start message
+    """
+
+    message_text = "Hello! Welcome to MoneyBot!"
+    message_text += "\n\n" + help_message().strip() # add help msg to start_message
+    logging.info(f"Start message: '{message_text}'")
+    return message_text
---
+++ utils.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.getLogger(__name__)
+
+
+def add_record(money : int, category : str):
+    """ TODO : add record saving
+    :returns: None
+    """
+    pass
